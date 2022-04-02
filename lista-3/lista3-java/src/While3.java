e020272744c281a7a4c2fbdea681bcc44375d336
Luiz Gonçalves <94024386+luizgnclvs@users.noreply.github.com>
2022-04-02 03:56:26 -0300

    Update While3.java

--- lista-3/lista3-java/src/While3.java
+++ lista-3/lista3-java/src/While3.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class While3 {
 
     static boolean isNumeric(String x) {
-		return x != null && x.matches("[0-9.-]+");
-	}
+	return x != null && x.matches("[0-9.-]+");
+    }
     
     public static void main(String[] args) {
         
@@ -33,8 +33,8 @@ public class While3 {
                     }
                 }
             } else {
-				System.out.println("Entrada inválida! Tente novamente\n");
-			}
+		System.out.println("Entrada inválida! Tente novamente\n");
+	    }
         }
 
         System.out.printf("\n\n\t%s \n%s\t\t\t%.1f \n%s\t%.0f \n%s\t%.2f%% \n%s\t%.0f \n%s\t%.2f%%", "Resultados da Operação", "Média Aritmética:", sum / count, "Quantidade de valores positivos:", pos, "Percentual de valores positivos:", (pos / count) * 100, "Quantidade de valores negativos:", neg, "Percentual de valores negativos:", (neg / count) * 100);
